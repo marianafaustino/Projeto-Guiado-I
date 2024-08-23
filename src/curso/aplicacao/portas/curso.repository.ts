0d7b4f6e3454bbd4bc4366e1ddf49f4a60c7c81c
marianafaustino <marifaustino@gmail.com>
2024-08-23 18:39:47 -0300

    feat: implementa metodos basicos do curso

--- src/curso/aplicacao/portas/curso.repository.ts
+++ src/curso/aplicacao/portas/curso.repository.ts
@@ -1,6 +1,7 @@
 import { Curso } from "src/curso/dominio/curso";
 
 export abstract class CursoRepository{
-    abstract Salvar(curso:Curso): Promise<Curso>
-    abstract listar(): Promise<Curso[]>
+    abstract Salvar(curso:Curso): void
+    abstract listar(): Curso[]
+    abstract buscarCursoId(id: string): Curso | undefined
 }
--- src/curso/infraestrutura/persistencia/local/curso.repository.ts
+++ src/curso/infraestrutura/persistencia/local/curso.repository.ts
@@ -1,14 +1,24 @@
 import { Injectable } from "@nestjs/common";
+import * as path from "path";
+import * as fs from 'fs';
 import { CursoRepository } from "src/curso/aplicacao/portas/curso.repository";
 import { Curso } from "src/curso/dominio/curso";
 
 @Injectable()
 export class LocalCursoRepository implements CursoRepository{
-    Salvar(curso: Curso): Promise<Curso> {
-        throw new Error('Método não implementado')
+    private readonly arquivoCurso = path.resolve('src/curso/infraestrutura/dados/curso.json')
+
+    Salvar(curso: Curso): void {
+        fs.writeFileSync(this.arquivoCurso, JSON.stringify(curso,null,2))
     }
 
-    listar(): Promise<Curso[]> {
-        throw new Error('Método não implementado')
+    listar(): Curso[] {
+        const dados = fs.readFileSync(this.arquivoCurso, 'utf-8');
+        return JSON.parse(dados) as Curso[];
+    }
+
+    buscarCursoId(id: string): Curso | undefined {
+        const cursos = this.listar()
+        return cursos.find(curso => curso.id === id)
     }
 }
